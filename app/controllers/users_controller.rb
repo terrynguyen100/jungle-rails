2657e27661962f9e7129a956c770b0cc4357f4a7
Terry Nguyen <terrynguyen100@gmail.com>
2023-06-30 00:04:30 -0600

    add session controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,12 @@ class UsersController < ApplicationController
     else
       redirect_to '/signup'
     end
-  end   
+  end 
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :categories, only: [:show]
 
   get '/about', to: 'about#index', as: 'about'
+  # authentication
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -76,21 +79,3 @@ Rails.application.routes.draw do
   #   end
 end
 
-# This is for authentication with bcrypt in rails
-
-GifVault::Application.routes.draw do
-    
-  # This route sends requests to our naked url to the *cool* action in the *gif* controller.
-  root to: 'gif#cool'
-  
-  # I've created a gif controller so I have a page I can secure later. 
-  # This is optional (as is the root to: above).
-  get '/cool' => 'gif#cool'
-  get '/sweet' => 'gif#sweet'
-
-  # These routes will be for signup. The first renders a form in the browse, the second will 
-  # receive the form and create a user in our database using the data given to us by the user.
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
-
-end
