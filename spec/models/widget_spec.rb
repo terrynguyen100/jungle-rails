368af3a7d9a56217649b3074d3c1037869a37db1
Terry Nguyen <terrynguyen100@gmail.com>
2023-07-03 15:12:40 -0600

    add 2 tests in widget model

--- spec/models/widget_spec.rb
+++ spec/models/widget_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Widget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# calling #id means calling an instance method of the widget model
+# start a new test-group with `describe`
+describe '#id' do
+  it 'should not exist for new records' do
+    @widget = Widget.new
+    expect(@widget.id).to be_nil 
+  end
+
+  it 'should be auto-assigned by AR for saved records' do
+    @widget = Widget.new
+
+    @widget.save!
+
+    expect(@widget.id).to be_present
+  end
 end
