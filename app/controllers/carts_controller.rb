c01a6099fc57a96e3eb651b5e9fb87a9e12e537d
Terry Nguyen <terrynguyen100@gmail.com>
2023-07-03 14:44:31 -0600

    add user auth

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,6 @@
 class CartsController < ApplicationController
+  
+  before_action :authorize
 
   def show
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ Rails.application.routes.draw do
   resources :categories, only: [:show]
 
   get '/about', to: 'about#index', as: 'about'
-  # authentication
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -22,6 +19,15 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
     resources :products, except: [:edit, :update, :show]
   end
+
+  # authentication
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
