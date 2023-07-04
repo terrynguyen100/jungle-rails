dcf6562b6ad93578a84678cbee8c61a3b67d3eea
Terry Nguyen <terrynguyen100@gmail.com>
2023-07-04 17:14:13 -0600

    add integration test

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,14 +1,33 @@
-describe('Home Page', () => {
-  beforeEach(() => {
-    // Visit the home page before each test
+describe('Integration Test', () => {
+  it("navigate from home page to product detail page by clicking on a product", () => {
     cy.visit('/');
+    cy.get('.product').first().click();
+    cy.url().should('contain', '/products/2');
+  });
+});
+
+describe("Product Detail Page", () => {
+  beforeEach(() => {
+    cy.visit("/products/1/");
   });
 
-  it("There is products on the page", () => {
-    cy.get(".products article").should("be.visible");
+  it("Displays the product name", () => {
+    cy.get(".product-detail h1").should("contain", "Giant Tea");
   });
 
-  it("There is 2 products on the page", () => {
-    cy.get(".products article").should("have.length", 2);
+  it("Displays the product description", () => {
+    cy.get(".product-detail p").should("contain", "The Giant Tea is an uncommon, medium-sized plant and can be found only in some tundras. It blooms twice a year, for 3 weeks.");
+  });
+
+  it("Displays the product image", () => {
+    cy.get(".product-detail .main-img").should("be.visible");
+  });
+
+  it("Displays the 'Add to Cart' button", () => {
+    cy.get(".product-detail .btn").should("contain", "Add");
+  });
+
+  it("Displays the stock quantity information", () => {
+    cy.get(".product-detail .quantity").should("contain", 0);
   });
 });
