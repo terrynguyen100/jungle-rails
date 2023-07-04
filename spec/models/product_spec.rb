05f5e79a9d24c9aecb5c76c638dd1d935447b9f1
Terry Nguyen <terrynguyen100@gmail.com>
2023-07-03 20:33:58 -0600

    update four-phase tests testing pattern

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,42 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
-    
-    it 'product is created successfully when all four fields are set' do
-      category = Category.new(name: 'Big Tree')
+    category = Category.new(name: 'Big Tree') 
+
+    it 'creates a product successfully when all four fields are set' do
       product = Product.new(name: 'Super Green', price_cents: 999, quantity: 99, category: category)
+
       expect(product.save).to be_truthy
     end
 
-    it 'should fail if name is nil' do
-      category = Category.new(name: 'Big Tree')
+    it 'fails to create a product when name is nil' do
       product = Product.new(name: nil, price_cents: 999, quantity: 99, category: category)
-      product.save
-      # need product.save first, without the save, there will be no `errors`
+
+      expect(product.save).to be_falsy
       expect(product.errors.full_messages).to include("Name can't be blank")
     end
-    
 
-    it 'should fail if price is nil' do
-      category = Category.new(name: 'Big Tree')
+    it 'fails to create a product when price is nil' do
       product = Product.new(name: 'Super Green', price_cents: nil, quantity: 99, category: category)
-      product.save
+
+      expect(product.save).to be_falsy
       expect(product.errors.full_messages).to include("Price can't be blank")
     end
 
-    it 'should fail if quantity is nil' do
-      category = Category.new(name: 'Big Tree')
-      product = Product.new(name: "Super Green", price_cents: 999, quantity: nil, category: category)
-      product.save
+    it 'fails to create a product when quantity is nil' do
+      product = Product.new(name: 'Super Green', price_cents: 999, quantity: nil, category: category)
+
+      expect(product.save).to be_falsy
       expect(product.errors.full_messages).to include("Quantity can't be blank")
     end
-    
-    it 'should fail if category is nil' do
-      product = Product.new(name: "Super Green", price_cents: 999, quantity: 99, category: nil)
-      product.save
+
+    it 'fails to create a product when category is nil' do
+      product = Product.new(name: 'Super Green', price_cents: 999, quantity: 99, category: nil)
+
+      expect(product.save).to be_falsy
       expect(product.errors.full_messages).to include("Category can't be blank")
     end
-
   end
 end
